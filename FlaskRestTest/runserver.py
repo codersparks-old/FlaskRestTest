ee3572e1d01108e351ed10cc159718c3f14d072d
coder sparks <codersparks@gmail.com>
2015-08-23 13:57:17 +0100

    Change to work on any ip address i.e. 0.0.0.0£

--- FlaskRestTest/runserver.py
+++ FlaskRestTest/runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from FlaskRestTest import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = "0.0.0.0"
     try:
         PORT = int('5555')
     except ValueError:
